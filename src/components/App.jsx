6b8f3b7cf549fa5b463b8388be8e0e7f5ccad437
KuhaiAnna <kuhai.anna@gmail.com>
2023-04-11 17:07:24 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,7 +7,7 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#000000'
       }}
     >
       React homework template
