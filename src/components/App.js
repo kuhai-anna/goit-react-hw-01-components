102b44ed94935ef7de337376eeddaf5afbd6a908
KuhaiAnna <kuhai.anna@gmail.com>
2023-04-13 17:54:55 +0300

    Add components to the App

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,16 @@
-import { Profile } from './ProfileCard/Profile';
+import { Profile } from './profile/Profile';
+import { Statistics } from './statistics/Statistics';
+import { FrindList } from './friendList/FriendList';
+import { TransactionHistory } from './transactions/TransactionHistory';
 import user from '../data/user.json';
+import data from '../data/data.json';
+import friends from '../data/friends.json';
+import transactions from '../data/transactions.json';
 
 export const App = () => {
   return (
     <div>
+      {/* профіль користувача */}
       <Profile
         username={user.username}
         tag={user.tag}
@@ -11,6 +18,12 @@ export const App = () => {
         avatar={user.avatar}
         stats={user.stats}
       />
+      {/* статистика завантажень */}
+      <Statistics title="Upload stats" stats={data} />
+      {/* список друзів */}
+      <FrindList items={friends} />
+      {/* історія транзакцій */}
+      <TransactionHistory items={transactions} />;
     </div>
   );
 };
